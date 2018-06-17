872d0607cdce676d2e14628b4c285fff96772445
Harsh Nandan <hnandan@scsolutions.com>
2018-06-17 14:59:06 -0700

    Inital implementation complete ... time to test and check

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,4 +1,5 @@
 #include "kalman_filter.h"
+#define _PI 3.14159265
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -22,21 +23,74 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-  TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-  TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+
+  MatrixXd PHt = P_ * Ht;
+
+  MatrixXd Si = S.inverse();
+  MatrixXd K = PHt * Si;
+
+//new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-  TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+
+  float rho = sqrt(px*px + py*py);
+  float theta = atan2(py, px);
+  float rhodot = 0;
+  if(rho<0.001){
+	  rhodot = 0;
+  }else{
+	  rhodot = (vx*py + vy*px)/rho;
+  }
+  VectorXd z_pred(3);
+  z_pred << rho, theta, rhodot;
+  VectorXd y = z - z_pred;
+
+  while (y[1] > _PI){
+     y[1] -= (2 * _PI);
+  }
+  while (y[1] < - _PI){
+     y[1] += (2 * _PI);
+  }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+
+  MatrixXd PHt = P_ * Ht;
+
+  MatrixXd Si = S.inverse();
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
